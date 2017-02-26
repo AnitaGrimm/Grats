9cd298b6d1decf0c3168b77e5ccda1cf63426d97
Maru <kotevode@icloud.com>
2017-02-26 22:07:53 +0500

    Добавлен IMessageTemplate

---
+++ Grats/Interfaces/IMessageTemplate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VkNet.Model;
+
+namespace Grats.Interfaces
+{
+    /// <summary>
+    /// Шаблонизированное сообщение
+    /// </summary>
+    interface IMessageTemplate
+    {
+        /// <summary>
+        /// Поля, используемые в шаблоне
+        /// </summary>
+        List<String> Fields { get; }
+        /// <summary>
+        /// Метод, возвращающий вычисленный текст на основе пользователя
+        /// </summary>
+        /// <param name="user">Пользователь VK</param>
+        /// <returns></returns>
+        string Substitute(User user);
+    }
+}
