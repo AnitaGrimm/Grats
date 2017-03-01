aafb4df2e7910168f37eec7ca27a9c5303f6f62b
Maru <kotevode@icloud.com>
2017-03-01 19:28:01 +0500

    Добавлена секция текущего пользователя + кнопка выхода

--- Grats/MainPage.xaml.cs
+++ Grats/MainPage.xaml.cs
@@ -8,29 +8,52 @@ using VkNet.Model;
 using VkNet.Model.RequestParams;
 using VkNet.Utils;
 using Windows.UI.Xaml.Controls;
+using System;
 
 // Документацию по шаблону элемента "Пустая страница" см. по адресу http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace Grats
 {
     /// <summary>
-    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// Главная страница приложения, содержит список друзей и поздравлений
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public VKCurrentUserViewModel Current { get; set; }
+
         public MainPage()
         {
             this.InitializeComponent();
             UpdateUI();
         }
 
+        /// <summary>
+        /// Обновление интерфейса
+        /// </summary>
         private void UpdateUI()
         {
+            // TODO: Подумать насчет переноса этого в параметры окна
+            UpdateCurrentUser();
+            // TODO: Добавить перехват исключений
             UpdateFriends();
+        }
+
+        private void UpdateCurrentUser()
+        {
+            var app = App.Current as App;
+            var usersAPI = app.VKAPI.Users;
+            Current = new VKCurrentUserViewModel(usersAPI.Get(
+                app.VKAPI.UserId.Value,
+                ProfileFields.FirstName | ProfileFields.LastName | ProfileFields.Photo200
+                ));
         }
 
         #region Получение списка друзей
 
+        /// <summary>
+        /// Получения списка друзей с VKAPI
+        /// </summary>
+        /// <returns>Список друзей</returns>
         public async Task<List<User>> FetchFriends()
         {
             var app = App.Current as App;
@@ -50,11 +73,16 @@ namespace Grats
             return result.ToList();
         }
 
+        /// <summary>
+        /// Обновление списка друзей
+        /// </summary>
         private async void UpdateFriends()
         {
             var app = App.Current as App;
+            // Тянем друзей с VK
             var friends = from friend in await FetchFriends()
                           select new VKUserViewModel(friend);
+            //Группируем по первой букве фамилии
             var groups = from item in friends
                          group item by item.ScreenName[0] into g
                          orderby g.Key
@@ -68,9 +96,16 @@ namespace Grats
                 group.AddRange(g.Friends);
                 collection.Add(group);
             }
+            // Обновляем источник данных
             FriendsGroupedByKey.Source = collection;
         }
 
         #endregion
+
+        private void LogoutMenuItem_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var app = App.Current as App;
+            app.SignOut();
+        }
     }
 }
--- Grats/ViewModels/VKCurrentUserViewModel.cs
+++ Grats/ViewModels/VKCurrentUserViewModel.cs
@@ -1,22 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using VkNet.Model;
 using Windows.UI.Xaml.Media.Imaging;
 
 namespace Grats.ViewModels
 {
-    public class VKUserViewModel
+    public class VKCurrentUserViewModel
     {
         public User User { get; set; }
         public String ScreenName { get; set; }
         public BitmapImage Photo { get; set; }
-        
-        public VKUserViewModel() { }
 
-        public VKUserViewModel(User user)
+        public VKCurrentUserViewModel() { }
+
+        public VKCurrentUserViewModel(User user)
         {
             this.User = user;
             this.ScreenName = user.LastName + " " + user.FirstName;
-            this.Photo = new BitmapImage(user.Photo100);
+            this.Photo = new BitmapImage(user.Photo200);
         }
     }
 }
